7cb5f898ec42160bb73ec1d5587cd045139d64c5
Dror Shriki <dshriki1974@gmail.com>
2017-07-06 23:21:28 +0300

    Aa

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dist = setup(
     test_suite="supervisor.tests",
     entry_points={
         'console_scripts': [
-            'scanner = portscan.portscan:main',
+            #'scanner = portscan.portscan:main',
         ],
     },
 )
