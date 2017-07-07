de4c93a125d0c1be70911e9b6f4b3c105ef18f87
Dror Shriki <dshriki1974@gmail.com>
2017-07-07 10:58:02 +0300

    aa

---
+++ portscan/portscan.py
@@ -0,0 +1,46 @@
+import socket
+import argparse
+
+def get_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument("dest")
+    parser.add_argument("start")
+    parser.add_argument("stop")
+
+    args = parser.parse_args()
+    return args
+
+
+
+class port_inspect:
+
+    def __init__(self,addr):
+        self.addr=addr
+    def run_ports(self,startport,endport):
+        print ("===============================")
+        print("Check ports for ip " + self.addr)
+        print ("===============================")
+        for port in range(startport,endport):
+            self.inspect_port(port)
+
+    def inspect_port(self,port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+
+        result=sock.connect_ex((self.addr,port))
+        if result==0:
+            print ("Port " + str(port) + " is open")
+        else:
+            print ("port " + str(port) + " is close")
+
+def main():
+    ARGS=get_args()
+    host_ip=ARGS.dest
+    host_ip="8.8.8.8"
+    google=port_inspect(host_ip)
+    start_port=int(ARGS.start)
+    stop_port=int(ARGS.stop)
+    google.run_ports(start_port,stop_port)
+
+if __name__ == "__main__":
+    main()
